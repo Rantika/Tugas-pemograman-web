16e2f88cb735bd22a9638317071131ad64a82a1f
rantika <79303778+Rantika@users.noreply.github.com>
2021-06-15 02:16:43 +0700

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$dbname   = 'db_bukawarung';
-
-$con = mysqli_connect($hostname, $username, $password, $dbname) or die ('Gagal terhubung ke database')
-
-?>
